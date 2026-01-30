938f01d96f729ee65c9f926b3c0f4ce0649aa4be
Divyang897 <divyangmistri6@gmail.com>
2026-01-30 17:26:22 +0530

    Add files via upload

---
+++ login_system.py
@@ -0,0 +1,10 @@
+USERNAME = "admin"
+PASSWORD = "1234"
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == USERNAME and password == PASSWORD:
+    print("Login Successful")
+else:
+    print("Login Failed")
